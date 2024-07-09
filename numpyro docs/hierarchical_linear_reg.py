a10ad81f56e07be7fa217642d5ad79aa39f9307a
Grace <lyuhan@gmail.com>
2024-07-09 12:40:05 +0100

    hierarchical example

--- numpyro docs/hierarchical_linear_reg.py
+++ numpyro docs/hierarchical_linear_reg.py
@@ -21,10 +21,11 @@ f, axes = plt.subplots(1, 3, figsize=(15,5))
 chart_patient("ID00007637202177411956430", axes[0])
 chart_patient("ID00009637202177434476278", axes[1])
 chart_patient("ID00010637202177584971671", axes[2])
-# plt.show()
+plt.show()
 
 # use partial pooling -> each individual's coefficients come from a common group distribution
 from jax import random
+import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS, Predictive
@@ -32,10 +33,10 @@ from numpyro.infer import MCMC, NUTS, Predictive
 
 def model(patient_code, Weeks, FVC_obs=None):
     # priors
-    mu_a = numpyro.sample("mu_a", dist.Normal(0, 500))
-    sigma_a = numpyro.sample("sigma_a", dist.HalfNormal(100))
-    mu_b = numpyro.sample("mu_b", dist.Normal(0, 3))
-    sigma_b = numpyro.sample("sigma_b", dist.HalfNormal(3))
+    mu_a = numpyro.sample("mu_a", dist.Normal(0.0, 500.0))
+    sigma_a = numpyro.sample("sigma_a", dist.HalfNormal(100.0))
+    mu_b = numpyro.sample("mu_b", dist.Normal(0.0, 3.0))
+    sigma_b = numpyro.sample("sigma_b", dist.HalfNormal(3.0))
 
     n_patients = len(np.unique(patient_code)) # num of unique patients
 
@@ -43,7 +44,7 @@ def model(patient_code, Weeks, FVC_obs=None):
         alpha = numpyro.sample("alpha", dist.Normal(mu_a, sigma_a))
         beta = numpyro.sample("beta", dist.Normal(mu_b, sigma_b))
 
-    sigma = numpyro.sample("sigma", dist.HalfNormal(100))
+    sigma = numpyro.sample("sigma", dist.HalfNormal(100.0))
     FVC_est = alpha[patient_code] + beta[patient_code] * Weeks
 
     with numpyro.plate("data", len(patient_code)):
@@ -52,8 +53,8 @@ def model(patient_code, Weeks, FVC_obs=None):
 from sklearn.preprocessing import LabelEncoder
 patient_encoder = LabelEncoder()
 train["patient_code"] = patient_encoder.fit_transform(train["Patient"].values) # number patients starting from 0
-FVC_obs = train["FVC"].values
-Weeks = train["Weeks"].values
+fvc_obs = train["FVC"].values
+weeks = train["Weeks"].values
 patient_code = train["patient_code"].values
 
 
@@ -62,5 +63,61 @@ mcmc = MCMC(
     num_warmup = 2000,
     num_samples = 2000
 )
-mcmc.run(random.PRNGKey(0), patient_code, Weeks, FVC_obs=FVC_obs)
-posterior_samples = mcmc.get_samples()
+mcmc.run(random.PRNGKey(1), patient_code, weeks, FVC_obs=fvc_obs)
+posterior_samples: dict = mcmc.get_samples()
+
+# observe parameters
+import arviz as az
+data = az.from_numpyro(mcmc)
+az.plot_trace(data, compact=True, figsize=(15,30))
+plt.show() 
+
+# plot results
+def create_prediction_template(unique_patient_df, weeks_series):
+    unique_patient_df["_temp"] = True
+    weeks = pd.DataFrame(weeks_series, columns=["Weeks"])
+    weeks["_temp"] = True
+    return unique_patient_df.merge(weeks, on="_temp").drop(["_temp"], axis = 1)
+
+patients = train[["Patient", "patient_code"]].drop_duplicates()
+start_week = -12
+end_week = 134
+predict_weeks = pd.Series(jnp.linspace(start_week, end_week))
+pred_template = create_prediction_template(patients, predict_weeks)
+
+
+patient_code = pred_template["patient_code"].values
+weeks = pred_template["Weeks"].values
+predictive = Predictive(model, posterior_samples, return_sites=["sigma", "obs"])
+samples_predictive = predictive(random.PRNGKey(0), patient_code, weeks, None)
+
+df = pred_template.copy()
+df["FVC_pred"] = samples_predictive["obs"].T.mean(axis=1)
+df["sigma"] = samples_predictive["obs"].T.std(axis=1)
+df["FVC_inf"] = df["FVC_pred"] - df["sigma"]
+df["FVC_sup"] = df["FVC_pred"] + df["sigma"]
+df = pd.merge(
+    df, train[["Patient", "Weeks", "FVC"]], how="left", on=["Patient", "Weeks"]
+)
+df = df.rename(columns={"FVC": "FVC_true"})
+
+def chart_patient_with_predictions(patient_id, ax):
+    data = df[df["Patient"] == patient_id]
+    x = data["Weeks"]
+    ax.set_title(patient_id)
+    ax.plot(x, data["FVC_true"], "o")
+    ax.plot(x, data["FVC_pred"])
+    ax = sns.regplot(x=x, y=data["FVC_true"], ax=ax, ci=None, line_kws={"color": "red"})
+    ax.fill_between(x, data["FVC_inf"], data["FVC_sup"], alpha=0.5, color="#ffcd3c")
+    ax.set_ylabel("FVC")
+
+
+f, axes = plt.subplots(1, 3, figsize=(15, 5))
+chart_patient("ID00007637202177411956430", axes[0])
+chart_patient("ID00009637202177434476278", axes[1])
+chart_patient("ID00011637202177653955184", axes[2])
+chart_patient_with_predictions("ID00007637202177411956430", axes[0])
+chart_patient_with_predictions("ID00009637202177434476278", axes[1])
+chart_patient_with_predictions("ID00011637202177653955184", axes[2])
+
+plt.show()
